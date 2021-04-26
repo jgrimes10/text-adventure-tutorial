fc8c96c831e08755774850efe8a9b6ddface04cf
Justin Grimes <grimesajustin@gmail.com>
2021-04-26 19:33:41 -0400

    Add InputResponse scene to show the player's input and the game's response. Add logic to instance a new copy of InstanceResponse and add it as a child of HistoryRows whenever text is entered.

--- Game.gd
+++ Game.gd
@@ -1,5 +1,18 @@
 extends Control
 
 
+# Get a copy of the InputResponse scene so we can make copies of it
+# whenever text is input to show in the GameInfo
+const InputResponse = preload("res://InputResponse.tscn")
+
+# Grab the history_rows node so we can make the new InputReponse
+# scenes children of it
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/HistoryRows
+
+
 func _on_Input_text_entered(new_text: String) -> void:
-	print(new_text)
+	# Create an instance of the InputResponse scene when
+	# text is entered
+	var input_response = InputResponse.instance()
+	# Add the InputResponse as a child of the history_rows node
+	history_rows.add_child(input_response)
