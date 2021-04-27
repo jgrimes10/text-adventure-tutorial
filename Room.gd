0734631c7827fb6502cc0ec9b8adf87489e4e17a
Justin Grimes <grimesajustin@gmail.com>
2021-04-27 09:38:36 -0400

    Make RoomName and RoomDescription show in the editor for each room.

--- Room.gd
+++ Room.gd
@@ -1,16 +1,29 @@
+tool # Make this script run in the editor
 extends PanelContainer
 class_name Room
 
 
-export (String) var room_name = "Room Name"
-export (String) var room_description = "This is the description of the room"
+export (String) var room_name = "Room Name" setget set_room_name
+export (String, MULTILINE) var room_description = "This is the description of the room" setget set_room_description
 
 var exits: Dictionary = {}          # A dictionary of the exits out of the room
 
 
+func set_room_name(new_name: String) -> void:
+	# Set the text in the editor to the room_name
+	$MarginContainer/Rows/RoomName.text = new_name
+	# Set the actual variable for room_name to the new_name
+	room_name = new_name
+
+
+func set_room_description(new_description: String) -> void:
+	# Set the text in the editor to the room_description
+	$MarginContainer/Rows/RoomDescription.text = new_description
+	# Set the actual variable for the room_description to the new_description
+	room_description = new_description
+
+
 func connect_exit(direction: String, room_to_connect: Room) -> void:
-	print("Current room ", room_name)
-	print("Target room ", room_to_connect.room_name)
 	match direction:
 		"north":
 			exits[direction] = room_to_connect
