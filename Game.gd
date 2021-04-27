1e758d721801e90fd299bb478b1a60d6a9c6c0d3
Justin Grimes <grimesajustin@gmail.com>
2021-04-26 21:24:37 -0400

    Add maximum amount of lines to be remembered in the GameInfo.

--- Game.gd
+++ Game.gd
@@ -5,6 +5,10 @@ extends Control
 # whenever text is input to show in the GameInfo
 const InputResponse = preload("res://InputResponse.tscn")
 
+# The maximum amount of previous entries that will be remembered
+# before the game starts "forgetting" the older lines
+export (int) var max_lines_remembered := 30
+
 # Grab the history_rows node so we can make the new InputReponse
 # scenes children of it
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
@@ -33,6 +37,10 @@ func handle_scrollbar_changed() -> void:
 
 
 func _on_Input_text_entered(new_text: String) -> void:
+	# Check to make sure there was text entered and it isn't an
+	# empty string, if it is empty, return and do nothing
+	if new_text.empty():
+		return
 	# Create an instance of the InputResponse scene when
 	# text is entered
 	var input_response = InputResponse.instance()
@@ -40,3 +48,13 @@ func _on_Input_text_entered(new_text: String) -> void:
 	input_response.set_text(new_text, "This is what the game will respond with.")
 	# Add the InputResponse as a child of the history_rows node
 	history_rows.add_child(input_response)
+	
+	# Check if there are more rows than the maximum amount of rows allowed
+	if history_rows.get_child_count() > max_lines_remembered:
+		# Figure out the number of rows over the maximum allowed
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		# Loop the amount of times equal to the number over
+		# the maximum allowed
+		for _i in range(rows_to_forget):
+			# Delete the oldest row
+			history_rows.get_child(0).queue_free()
