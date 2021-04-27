f43b806ed8566009844820cf95f794f02c67c895
Justin Grimes <grimesajustin@gmail.com>
2021-04-26 20:54:34 -0400

    Add logic to scroll InputResponse to the bottom when new input is added.

--- Game.gd
+++ Game.gd
@@ -7,7 +7,29 @@ const InputResponse = preload("res://InputResponse.tscn")
 
 # Grab the history_rows node so we can make the new InputReponse
 # scenes children of it
-onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
+# Grab the scroll node so we can grab the v_scrollbar on it
+onready var scroll = $Background/MarginContainer/Rows/GameInfo/Scroll
+# Grab the v_scrollbar so we can scroll down to the bottom when new input is sent
+onready var scrollbar = scroll.get_v_scrollbar()
+
+var max_scroll_length := 0         # The current length of the ScrollContainer
+
+
+func _ready() -> void:
+	# Connect to the changed event on the vertical scrollbar
+	scrollbar.connect("changed", self, "handle_scrollbar_changed")
+	# Set the max_scroll_length to its current max value
+	max_scroll_length = scrollbar.max_value
+
+
+func handle_scrollbar_changed() -> void:
+	# Check if the length of the scrollbar has changed
+	if max_scroll_length != scrollbar.max_value:
+		# Update the max_scroll_length to match
+		max_scroll_length = scrollbar.max_value
+		# Scroll all the way to the bottom of the ScrollContainer
+		scroll.scroll_vertical = max_scroll_length
 
 
 func _on_Input_text_entered(new_text: String) -> void:
